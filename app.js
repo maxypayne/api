2cca61da760fcf113a1df030a398130b7b9cdb38
maxypayne <maximlucov@gmail.com>
2020-05-09 19:51:29 +0200

    ko

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ require('dotenv').config();
 require('./config/mongo');
 const app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://maximlucov.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 app.use(logger('dev'));
